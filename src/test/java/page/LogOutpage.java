1ca54872751040162ac4c12902af4fba17dbc8ea
harshusai <harshithanaidu25@gmail.com>
2025-12-29 06:27:29 +0530

    Pagevalidation

--- src/test/java/page/LogOutpage.java
+++ src/test/java/page/LogOutpage.java
@@ -1,5 +1,31 @@
 package page;
 
-public class LogOutpage {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LogOutpage {
+	
+	 WebDriver driver;
+	
+	
+
+	public  LogOutpage(WebDriver driver) {
+		this.driver = driver;
+		 
+	}
+	 // ❌ Error message locator
+    By errorMsg = By.xpath("//div[@role='alert']//p");
+
+
+
+    // get error message text
+    public String getErrorMessage() {
+        return driver.findElement(errorMsg).getText();
+    }
 }
---
+++ src/test/java/testcase/InvalidLoginTestCase.java
@@ -0,0 +1,34 @@
+package testcase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.BaseClass;
+import page.LogOutpage;
+import page.LoginPage;
+
+public class InvalidLoginTestCase extends BaseClass {
+
+	@Test
+	public void invalidLoginTest() {
+
+		LoginPage lp = new LoginPage(driver);
+		lp.login("Admin", "wrongpassword");
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(
+				ExpectedConditions.visibilityOfElementLocated(org.openqa.selenium.By.xpath("//div[@role='alert']//p")));
+
+		LogOutpage lp1 = new LogOutpage(driver);
+
+		String errorText = lp1.getErrorMessage();
+		System.out.println("Actual error text is: ---> " + errorText);
+
+		Assert.assertEquals(errorText , "CSRF token validation failed");
+
+	}
+}
